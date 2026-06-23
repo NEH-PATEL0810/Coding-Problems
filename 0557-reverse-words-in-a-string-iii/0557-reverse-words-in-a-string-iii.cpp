3c4ecb39da1436623f37a65e899acf81942f2eb3
Neh Patel <nehp00417@gmail.com>
2026-06-23 17:05:00 +0530

    Time: 0 ms (100%), Space: 12.1 MB (89.07%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,32 +1,13 @@
 class Solution {
 public:
-string reverseW(string str,int start,int end){
-    int i=start;
-    int j=end-1;
-    while(i<=j){
-        swap(str[i],str[j]);
-        i++;
-        j--;
-    }
-    return str;
-}
     string reverseWords(string s) {
-        // string result="";
-        int i=0;
-        int j=s.length()-1;
-        int start = 0 ;
-        int end = 0;
-
-        while(i<=j){
-           if(s[i]==' '){
-            end = i;
-            s=reverseW(s,start,end);
-            start = end +1;
-           } 
-           i++;
+        int l = 0;
+        for (int i = 0; i <= s.size(); i++) {
+            if (i == s.size() || s[i] == ' ') {
+                reverse(s.begin() + l, s.begin() + i);
+                l = i + 1;
+            }
         }
-        s = reverseW(s,start,s.length());
         return s;
-
     }
 };
