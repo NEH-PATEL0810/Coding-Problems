ac4fe8095afcfaa4645da2cc98defce813f02cb7
Neh Patel <nehp00417@gmail.com>
2026-06-29 18:51:24 +0530

    Time: 0 ms (100%), Space: 9.3 MB (15.23%) - LeetHub

--- 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool isVowel(char ch) {
-        string vowel = "aeiou";
-        return (vowel.find(ch) != string::npos);
-    }
+    // bool isVowel(char ch) {
+    //     string vowel = "aeiou";
+    //     return (vowel.find(ch) != string::npos);
+    // }
     int vowelConsonantScore(string s) {
         s.erase(remove_if(s.begin(), s.end(),
                           [](unsigned char c) { return !isalpha(c); }),
                 s.end());
-
+        string v = "aeiou";
         int vowel = 0;
         int consonant = 0;
 
         for (char c:s){
-            if(isVowel(c)){
+            if(v.find(c) != string::npos){
                 vowel +=1;
             }
             else{
