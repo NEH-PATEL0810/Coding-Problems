8e60eed389753445e623959f63287395f6e4e5f0
Neh Patel <nehp00417@gmail.com>
2026-07-01 05:44:33 +0530

    Time: 0 ms (100%), Space: 35.1 MB (93.51%) - LeetHub

---
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
+        int a = *min_element(nums1.begin(),nums1.end());
+        int b = *min_element(nums2.begin(),nums2.end());
+
+        if(a>b) return -(a-b);
+
+        return b-a;
+
+    }
+};
